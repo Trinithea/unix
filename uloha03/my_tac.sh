2a8b184da3491d2cd7c3c2eadc1411beb9e98ecc
Michaela Mazna <mysakmazplus@gmail.com>
2019-04-09 11:03:29 +0200

    zapsani ty copypasty do funkce

--- uloha03/my_tac.sh
+++ uloha03/my_tac.sh
@@ -4,29 +4,25 @@ $( cat > /tmp/vstup)
 i=0
 NumOfRows=$( cat /tmp/vstup | wc -l )
 
-if [ "$1" == "-n" ]; then
-	while [ $i -lt $(( $NumOfRows / $2 )) ]
+Func(){
+while [ $i -lt $(( $NumOfRows / $1 )) ]
         do
-                cat /tmp/vstup | tail -n $2
-                CurNOR=$((  $(cat /tmp/vstup | wc -l) - $2))
+                cat /tmp/vstup | tail -n $1
+                CurNOR=$((  $(cat /tmp/vstup | wc -l) - $1))
                 $( cat /tmp/vstup | head -n $CurNOR > /tmp/pom)
                 $( cat /tmp/pom > /tmp/vstup )
                 i=$(( $i + 1 ))
         done
+}
+
+if [ "$1" == "-n" ]; then
+	Func $2
 	if [ $(( $NumOfRows % $2 )) != 0 ]; then
 		cat /tmp/vstup
 	fi
 	
 elif [ -z "$1" ]; then
-	
-	while [ $i -lt $NumOfRows ]
-	do 
-		echo $(cat /tmp/vstup | tail -n 1)
-		CurNOR=$((  $(cat /tmp/vstup | wc -l) - 1))
-		$( cat /tmp/vstup | head -n $CurNOR > /tmp/pom)
-		$( cat /tmp/pom > /tmp/vstup )
-		i=$(( $i + 1 ))
-	done
+	Func 1
 fi
 rm /tmp/vstup
 rm /tmp/pom
