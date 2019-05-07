9af8b728a35e3ee55baea869488a3d1bf9ca197f
Michaela Mazna <mysakmazplus@gmail.com>
2019-05-07 11:37:34 +0200

    funkcni

---
+++ uloha06_2.sh
@@ -0,0 +1,9 @@
+
+sort -k 2 --field-separator=';' countrycodes_en.csv > couSort.csv
+sort -k 3 --field-separator=';' kodyzemi_cz.csv > kodSort.csv
+join -t ";" -1 2 -2 3 couSort.csv kodSort.csv > Sorted
+awk -F ";" '{ if ( $2 == $NF ) {
+		gsub("\"","",$2)
+		print $2
+	}
+}' Sorted
